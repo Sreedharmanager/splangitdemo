cfaf88cbc8c11fe30dc2723dff6c98e0af9d088d
NavyaVeeradasu <NavyaVeeradasu@SPL-LP-085>
2024-07-22 11:49:53 +0530

    committing base project

---
+++ src/main/java/com/splan/demoforgit/DemoWebResource.java
@@ -0,0 +1,22 @@
+package com.splan.demoforgit;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoWebResource {
+	
+	
+	
+	@GetMapping("/getMessage/{name}")
+	public ResponseEntity<?> getMessage(@PathVariable("name") String name) {
+		return new ResponseEntity<>(" Hyy " + name, HttpStatus.OK);
+
+	}
+
+}
